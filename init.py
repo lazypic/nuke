9dcad85cb05141280fcbaf48804e0e2b94b14ec4
kim hanwoong <khw7096@gmail.com>
2018-12-12 23:26:43 +0900

    add pluginAppendPath

--- init.py
+++ init.py
@@ -2,5 +2,10 @@
 import os
 import nuke
 
+nuke.pluginAppendPath("./gizmos")
+nuke.pluginAppendPath("./images")
+nuke.pluginAppendPath("./lib")
+nuke.pluginAppendPath("./scripts")
+
 nukePath = os.environ['NUKE_PATH']
 nuke.ViewerProcess.register("AlexaV3LogC", nuke.createNode, ( "Vectorfield", "vfield_file %s/luts/AlexaV3_K1S1_LogC2Video_Rec709_EE_nuke3d.cube colorspaceIn AlexaV3LogC" % (nukePath)))
