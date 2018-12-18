09db17cf696fd08d6c67ae795215e684b158631c
kim hanwoong <khw7096@gmail.com>
2018-12-19 00:16:57 +0900

    add my cam lust

--- init.py
+++ init.py
@@ -9,3 +9,10 @@ nuke.pluginAppendPath("./scripts")
 
 nukePath = os.environ['NUKE_PATH']
 nuke.ViewerProcess.register("AlexaV3LogC", nuke.createNode, ( "Vectorfield", "vfield_file %s/luts/AlexaV3_K1S1_LogC2Video_Rec709_EE_nuke3d.cube colorspaceIn AlexaV3LogC" % (nukePath)))
+
+# black magic camera : https://www.convergent-design.com/preset-luts
+nuke.ViewerProcess.register("v2 Blackmagic Cinema Camera to Rec709", nuke.createNode, ( "Vectorfield", "vfield_file %s/luts/BMD_CC_EE_FILM_V2.cube colorspaceIn rec709" % (nukePath)))
+nuke.ViewerProcess.register("v2 Blackmagic Production Camera4K to Rec709", nuke.createNode, ( "Vectorfield", "vfield_file %s/luts/BMD_PC_EE_FILM_V2.cube colorspaceIn rec709" % (nukePath)))
+nuke.ViewerProcess.register("v1 Blackmagic Production Camera4K to Rec709", nuke.createNode, ( "Vectorfield", "vfield_file %s/luts/BMD_PC_EE_FILM_V1.cube colorspaceIn rec709" % (nukePath)))
+nuke.ViewerProcess.register("v1 Blackmagic Cinema Camera to Rec709", nuke.createNode, ( "Vectorfield", "vfield_file %s/luts/BMD_CC_EE_FILM_V1.cube colorspaceIn rec709" % (nukePath)))
+
