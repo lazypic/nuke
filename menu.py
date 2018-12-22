fd7644ad146baeaec527cb1774b5dd7a9fc907cb
kim hanwoong <khw7096@gmail.com>
2018-12-22 11:45:14 +0900

    add openfile

--- menu.py
+++ menu.py
@@ -1,10 +1,14 @@
 import nukescripts
+import openfile
+
 # Addon Menu
 mb = menubar.addMenu("Lazypic")
 mb.addCommand("Issue_and_Bugs", "nukescripts.start('https://github.com/lazypic/nuke/issues')")
 mb.addCommand("Slack", "nukescripts.start('https://lazypic.slack.com')")
 mb.addCommand("Clip Library", "nukescripts.start('https://www.youtube.com/channel/UC0L_YtB4PWSkOwp2m9587MA/playlists?view_as=subscriber')")
 mb.addCommand("Youtube", "nukescripts.start('https://www.youtube.com/channel/UCHsxW9hros1VlKKd4HAvG8A')")
+mb.addCommand("-","","")
+mb.addCommand("OpenFile", "openfile.openfile()", "F8", shortcutContext=2)
 
 # Gizmo
 tb = nuke.toolbar("Nodes")
