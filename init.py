3f21a0e7fd929bb03f9f8642fa62afb1cfc83cdc
kim hanwoong <khw7096@gmail.com>
2019-02-16 01:31:43 +0900

    add new lut

--- init.py
+++ init.py
@@ -17,7 +17,7 @@ nuke.pluginAddPath("./scripts", addToSysPath=True)
 
 nukePath = os.environ['NUKE_PATH']
 nuke.ViewerProcess.register("AlexaV3LogC_OLD", nuke.createNode, ( "Vectorfield", "vfield_file %s/luts/AlexaV3_K1S1_LogC2Video_Rec709_EE_nuke3d.cube colorspaceIn AlexaV3LogC" % (nukePath)))
-nuke.ViewerProcess.register("AlexaV3LogC", nuke.createNode, ( "Vectorfield", "vfield_file %s/luts/ARRI_LogC2Video_709_davinci3d.cube colorspaceIn AlexaV3LogC" % (nukePath)))
+nuke.ViewerProcess.register("AlexaV3LogC", nuke.createNode, ( "Vectorfield", "vfield_file %s/luts/ARRI_LogC2Video_Classic709.cube colorspaceIn AlexaV3LogC" % (nukePath)))
 
 # We are using Black Magic Pocket Cinema Camera
 # Black Magic Camera : https://www.convergent-design.com/preset-luts
