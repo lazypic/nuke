b449f91b20c5ba0f598f095c87953ee5788a30a5
kim hanwoong <khw7096@gmail.com>
2019-02-20 00:49:19 +0900

    change class

--- scripts/checkenv.py
+++ scripts/checkenv.py
@@ -2,18 +2,25 @@
 import os
 from PySide2.QtWidgets import *
 
+class CheckEnv(QWidget):
+	layout = QVBoxLayout()
+	def __init__(self):
+		super(CheckEnv, self).__init__()
+		self.setLayout(self.layout)
+		self.setEnv()
+
+	def setEnv(self):
+		envs = ["USER","OCIO","NUKE_PATH","NUKE_FONT_PATH"]
+		for e in envs:
+			self.layout.addWidget(QLabel("$%s : %s" % (e, os.environ.get(e,""))))
+
 def main():
 	global customApp
 	try:
 		customApp.close()
 	except:
 		pass
-	customApp = QWidget()
-	layout = QVBoxLayout()
-	envs = ["USER","OCIO","NUKE_PATH","NUKE_FONT_PATH"]
-	for e in envs:
-		layout.addWidget(QLabel("$%s : %s" % (e, os.environ.get(e,""))))
-	customApp.setLayout(layout)
+	customApp = CheckEnv()
 	try:
 		customApp.show()
 	except:
